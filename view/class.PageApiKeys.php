48c9787ce6eb058dbe29b78dbb0e416ca1210b34
Joeri Hermans <joeri@joerihermans.com>
2017-02-18 19:15:03 +0100

    Update Swagger documentation

--- view/class.PageApiKeys.php
+++ view/class.PageApiKeys.php
@@ -29,6 +29,34 @@ class PageApiKeys extends AbstractApiPage
         }
     }
 
+    /**
+     * @SWG\Get(
+     *     path="/api/keys",
+     *     summary="Returns details of keys selected by paging.",
+     *     tags={"All", "Keys"},
+     *     produces={"application/json"},
+     *     @SWG\Parameter(
+     *         in="query",
+     *         name="lastId",
+     *         description="Last id on the previous page, by default this is 0",
+     *         required=false,
+     *         default=0,
+     *         type="integer"
+     *     ),
+     *     @SWG\Parameter(
+     *         in="query",
+     *         name="numEntries",
+     *         description="Number of entries in the page. By default this value is set to 10, however, a 100 entries limit is enforced.",
+     *         required=false,
+     *         default=10,
+     *         type="integer"
+     *     ),
+     *     @SWG\Response(
+     *         response="200",
+     *         description="Returns the requested keys."
+     *     )
+     * )
+     */
     private function handleGetRequest()
     {
         // TOOD Implement.
